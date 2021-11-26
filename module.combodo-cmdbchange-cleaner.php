25ca9250cb1175d7e91ddf1f79d1e778c80cd6e9
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-26 17:49:20 +0100

    :bookmark: Update module version from 1.0.1 to 1.0.2

--- module.combodo-cmdbchange-cleaner.php
+++ module.combodo-cmdbchange-cleaner.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-cmdbchange-cleaner/1.0.1',
+	'combodo-cmdbchange-cleaner/1.0.2',
 	array(
 		// Identification
 		//
